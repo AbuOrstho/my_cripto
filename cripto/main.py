1eb0b5c46ddd1c739d6ee128bd0e4cf6fa426aff
AbuOrstho <85187741+AbuOrstho@users.noreply.github.com>
2024-02-17 18:22:02 +0300

    Update main.py

--- cripto/main.py
+++ cripto/main.py
@@ -1,6 +1,8 @@
 import time
 import asyncio
 import sys
+
+# Предполагаем, что эти импорты относятся к вашим модулям и они уже адаптированы под асинхронный запуск
 from Coinex.Coinex import coinex
 from LBank.LBank import lbank
 from BitMart.BitMart import bitmart
@@ -8,16 +10,23 @@ from Huobi.Huobi import huobi
 from MEXC.MEXC import mexc
 
 
-start_time = time.time()
+async def main():
+    start_time = time.time()
 
-if sys.platform == 'win32':
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    # Предполагаем, что run методы асинхронные и можно вызвать await на них
+    await asyncio.gather(
+        coinex.run(),
+        lbank.run(),
+        bitmart.run(),
+        huobi.run(),
+        mexc.run()
+    )
 
-# Ваш код с использованием asyncio тут
-coinex.run()
-lbank.run()
-bitmart.run()
-huobi.run()
-mexc.run()
+    print(f"Время выполнения: {time.time() - start_time} секунд.")
 
-print(time.time() - start_time)
+
+if __name__ == "__main__":
+    if sys.platform == 'win32':
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+    asyncio.run(main())
